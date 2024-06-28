12aae55525fa697275432061533103d64c078eab
ilgusto <devoluca@gmail.com>
2024-06-28 23:26:19 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -114,3 +114,12 @@ function loadComments() {
 document.addEventListener('DOMContentLoaded', function() {
   loadComments();
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.getElementById('hamburger');
+  const navMenu = document.getElementById('nav-menu');
+
+  hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
+});
